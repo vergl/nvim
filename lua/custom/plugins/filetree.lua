d2225c1028983f8e8391d3723cc64558efeb2cf8
vergl <stanislav.giley@gmail.com>
2024-03-25 11:51:51 +0500

    initial commit

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,25 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      event_handlers = {
+
+        {
+          event = 'file_opened',
+          handler = function(file_path)
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
+    }
+  end,
+}
