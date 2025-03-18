af57175863c367c5449e31d66a1b9becdc7e04b6
vergl <stanislav.giley@gmail.com>
2025-03-18 10:26:29 +0300

    update diffview configuration

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -5,7 +5,10 @@ return {
     vim.keymap.set('n', '<leader>Gdo', ':DiffviewOpen<CR>', { desc = '[G]it: [D]iffview [O]pen' })
     vim.keymap.set('n', '<leader>Gdc', ':DiffviewClose<CR>', { desc = '[G]it: [D]iffview [C]lose' })
     vim.keymap.set('n', '<leader>Gdh', ':DiffviewFileHistory<CR>', { desc = '[G]it: [D]iffview File [H]istory' })
+    vim.keymap.set('n', '<leader>Gdo', ':DiffviewFileHistory --follow %<CR>', { desc = '[G]it: [D]iffview File History (F[O]llow)' })
+    vim.keymap.set('n', '<leader>Gdl', ':.DiffviewFileHistory --follow<CR>', { desc = '[G]it: [D]iffview File History ([L]ine)' })
     vim.keymap.set('n', '<leader>Gdf', ':DiffviewToggleFiles<CR>', { desc = '[G]it: [D]iffview Toggle [F]iles' })
+    vim.keymap.set('v', ',Gdo', "<Esc><Cmd>'<,'>DiffviewFileHistory --follow<CR>", { desc = '[G]it: [D]iffview File History F[O]llow (visual)' })
     require('diffview').setup {
       use_icons = true, -- Включает иконки (если поддерживается)
       enhanced_diff_hl = true, -- Улучшенная подсветка diff
